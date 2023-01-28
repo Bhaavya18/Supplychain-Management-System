1f0eeab9a0fff3dc6da1829600e44404e23aa460
Aditi Gupta <70718967+Aditi1025@users.noreply.github.com>
2023-01-28 10:33:58 +0530

    admin-panel

---
+++ admin/src/datatablesource.js
@@ -0,0 +1,62 @@
+export const businessColumns = [
+  { field: "_id", headerName: "ID", width: 70 },
+  {
+    field: "name",
+    headerName: "Business Name",
+    width: 230,
+    renderCell: (params) => {
+      return (
+        <div className="cellWithImg">
+          <img className="cellImg" src={params.row.img || "https://i.ibb.co/MBtjqXQ/no-avatar.gif"} alt="avatar" />
+          {params.row.name}
+        </div>
+      );
+    },
+  },
+
+  {
+    field: "country",
+    headerName: "Country",
+    width: 100,
+  },
+  
+];
+
+export const orderColumns = [
+  { field: "_id", headerName: "ID", width: 70 },
+  {
+    field: "to",
+    headerName: "To Country",
+    width: 100,
+  },
+  {
+    field: "from",
+    headerName: "From Country",
+    width: 100,
+  },
+  {
+    field: "price",
+    headerName: "price",
+    width: 100,
+  },
+  {
+    field: "qty",
+    headerName: "Quantity",
+    width: 100,
+  },
+  {
+    field: "commodityName",
+    headerName: "Commodity Name",
+    width: 150,
+  },
+  {
+    field: "volume",
+    headerName: "Volume",
+    width: 150,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 150,
+  },
+];
---
+++ admin/src/formSource.js
@@ -0,0 +1,59 @@
+export const businessInputs = [
+    {
+      id: "name",
+      label: "Business Name",
+      type: "text",
+      placeholder: "john_doe",
+    },
+    {
+      id: "password",
+      label: "Password",
+      type: "password",
+    },
+    {
+      id: "country",
+      label: "Country",
+      type: "text",
+      placeholder: "USA",
+    },
+  ];
+
+  export const orderInputs = [
+    {
+      id: "to",
+      label: "To",
+      type: "text",
+      placeholder: "Country Name",
+    },
+    {
+      id: "from",
+      label: "From",
+      type: "text",
+      placeholder: "Country Name",
+    },
+    {
+      id: "price",
+      label: "Price",
+      type: "text",
+      placeholder: "Add Price",
+    },
+    {
+      id: "qty",
+      label: "Quantity",
+      type: "text",
+      placeholder: "100",
+    },
+    {
+      id: "commodityName",
+      label: "Commodity Name",
+      type: "text",
+      placeholder: "NYC",
+    },
+    {
+      id: "volume",
+      label: "Volume",
+      type: "text",
+      placeholder: "m3",
+    }
+  ];
+  
